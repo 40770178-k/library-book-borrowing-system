c17331e249bbc8a6a4f25da29c55e8db28dc3964
40770178-k <153083964+40770178-k@users.noreply.github.com>
2025-10-15 19:00:09 +0300

    added review feature

--- library_app/admin.py
+++ library_app/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
+from .models import Review
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('book', 'member', 'rating', 'created_at')
+    list_filter = ('rating', 'created_at', 'book')
+    search_fields = ('book__title', 'member__fullname', 'comment')
 from .models import Book,Member,BorrowedBook
 
 # Register your models here.
