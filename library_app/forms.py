38156d403a3433c13e430d76d49ae73c937332db
40770178-k <153083964+40770178-k@users.noreply.github.com>
2025-10-15 19:00:28 +0300

    added review feature

--- library_app/forms.py
+++ library_app/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Book, Member, BorrowedBook
+from .models import Review
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
@@ -26,6 +27,16 @@ class signupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['password1', 'password2']
+
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = ['rating', 'comment']
+        widgets = {
+            'rating': forms.NumberInput(attrs={'min': 1, 'max': 5, 'class': 'form-control'}),
+            'comment': forms.Textarea(attrs={'rows': 3, 'class': 'form-control'}),
+        }
 
 
         
